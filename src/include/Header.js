a7c67e599fb843748994251a33538c2e13dd337e
Hayoung Cho <hayoungcho826@gmail.com>
2022-01-04 23:09:23 +0900

    Add files via upload

--- src/include/Header.js
+++ src/include/Header.js
@@ -1,44 +1,44 @@
 import './header-style.scss';
 
-
+// li 목록들 받아와서 반복문 돌리기
 
 function Header() {
   return (
     <header>
-      <div className="innerContainer">
+      <nav className='innerContainer'>
         <h1>Portmeirion</h1>
         <ul>
-          <li>New
-            <ul className="subMenu new">
+          <li>New Arrival
+            <ul className='new subMenu'>
               <li>Botanic Garden</li>
               <li>Botanic Garden Bouquet</li>
               <li>Botanic Garden Harmony Accents</li>
               <li>Botanic Garden Harmony Bakeware</li>
               <li>Botanic Garden Harmony Papilio</li>
               <li>Sophie Conran Floret & Arbor</li>
-              <img src="./img/collection.jpg"></img>
+              <img src="http://via.placeholder.com/300x400" alt='collectionPicture'/>
             </ul>
           </li>
           <li>Collections
-            <ul className="subMenu collections">
+            <ul className='collections subMenu'>
               <li>Botanic Garden</li>
               <li>Botanic Garden Bouquet</li>
               <li>Botanic Garden Harmony Accents</li>
               <li>Botanic Garden Harmony Bakeware</li>
               <li>Botanic Garden Harmony Papilio</li>
               <li>Sophie Conran Floret & Arbor</li>
-              <img src="./img/collection.jpg"></img>
+              <img src="http://via.placeholder.com/300x400" alt='collectionPicture'/>
             </ul>
           </li>
           <li>About Us</li>
           <li>Contact Us</li>
         </ul>
         <ul>
-          <li>Log in</li>
+          <li>Login</li>
           <li>Cart</li>
           <li>Wishlist</li>
         </ul>
-      </div>
+      </nav>
     </header>
   );
 }
