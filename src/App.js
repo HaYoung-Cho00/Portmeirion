daf4d8c915642a474b1af847d5d062e3c29c85a9
Hayoung Cho <hayoungcho826@gmail.com>
2022-01-04 23:10:00 +0900

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './include/Header';
+import Footer from './include/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Footer></Footer>
     </div>
   );
 }
