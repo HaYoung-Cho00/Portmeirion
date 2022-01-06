1ee411961a3da6889397daabebbe1e7a160dc08b
Hayoung Cho <hayoungcho826@gmail.com>
2022-01-06 17:47:00 +0900

    Add files via upload

--- src/main/main-index.js
+++ src/main/main-index.js
@@ -1,5 +1,5 @@
 import './main-style.scss';
-
+import { IoIosArrowForward, IoIosArrowBack } from 'react-icons/io';
 
 function Main() {
   return(
@@ -10,22 +10,12 @@ function Main() {
       <section className="innerContainer">
         <article id="new">
           <div className="textArea">
-            <h1>New Arrival</h1>
-            <p>WE THINK YOU'LL LOVE THESE</p>
+            <h1>NEW ARRIVAL</h1>
+            <p>We think you'll love these</p>
           </div>
-          <ul>
-            <li><img src="http://via.placeholder.com/300X300" alt="newItem"/></li>
-            <li><img src="http://via.placeholder.com/300X300" alt="newItem"/></li>
-            <li><img src="http://via.placeholder.com/300X300" alt="newItem"/></li>
-            <li><img src="http://via.placeholder.com/300X300" alt="newItem"/></li>
-          </ul>
-        </article>
-        <article id="collections">
-          <div className="textArea">
-            <h1>Collections</h1>
-            <p>DISCOVER THE ART OF THE EVERYDAY</p>
-          </div>
-          <div id="collectionArea">
+          <div id="newItems">
+            <IoIosArrowBack id='leftArrow' />
+            <IoIosArrowForward id='rightArrow' />
             <div>
               <p>
                 Loved and recognized worldwide. Botanic Garden is a classic floral pattern that is as fresh and exciting today as it was in 1972.
@@ -39,6 +29,96 @@ function Main() {
             </div>
           </div>
           <h3>BOTANIC GARDEN</h3>
+        </article>
+        <article id="collections">
+          <div className="textArea">
+            <h1>FEATURED COLLECTIONS</h1>
+            <p>Discover the art of the everyday</p>
+          </div>
+          <div id='gallery'>
+            <img id='coll1' src='http://via.placeholder.com/350X500' alt='collections'></img>
+            <div id='desc1' className='desc'>
+              <h1>Collection Name1</h1>
+              <p>
+                Temporibus autem quibusdam et aut officiis debitis 
+                aut rerum necessitatibus saepe eveniet ut et voluptates 
+                repudiandae sint et Itaque earum rerum hic tenetur a sapiente 
+                delectus, At vero ntorustinctio. Nam libero tempore, cum soluta 
+                nobis est eligendi ut aut reiciendis voluptatibus maiores alias 
+                consequatur aut perferendis doloribus asperiores repellat.
+              </p>
+              <button>Read More</button>
+            </div>
+            <img id='coll2' src='http://via.placeholder.com/710X500' alt='collections'></img>
+            <div id='desc2' className='desc'>
+              <h1>Collection Name2</h1>
+              <p>
+                At vero ntorustinctio. Nam libero tempore, cum soluta nobis est 
+                eligendi aut officiis debitis aut rerum necessitatibus saepe 
+                eveniet ut et voluptates repudiandae sint et molestiae non 
+                recusandae. Itaque earum rerum hic tenetur a sapiente delectus, 
+                ut aut reiciendis voluptatibus maiores alias consequatur aut 
+                perferendis doloribus asperiores repellat.
+              </p>
+              <button>Read More</button>
+            </div>
+            <img id='coll3' src='http://via.placeholder.com/760X300' alt='collections'></img>
+            <div id='desc3' className='desc'>
+              <h1>Collection Name3</h1>
+              <p>
+                At vero qui animi, id est assumenda est, omnis dolor repellendus. 
+                Temporibus autem quibusdam et aut officiis debitis aut rerum 
+                necessitatibus saepe eveniet ut et voluptates repudiandae sint et 
+                molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente 
+                delectus, ut aut reiciendis voluptatibus maiores alias consequatur 
+                aut perferendis doloribus asperiores repellat.
+              </p>
+              <button>Read More</button>
+            </div>
+            <div id='readmore'>
+              READ MORE 
+            </div>
+          </div>
+        </article>
+        <article id='recommendation'>
+          <div className="textArea">
+            <h1>PICKED FOR YOU</h1>
+            <p>Treat yourself. Treat a friend. Treat a loved one.</p>
+          </div>
+          <ul>
+            <li>
+              <img src="http://via.placeholder.com/300X300" alt='bowl' />
+              <ul className='pickedDesc'>
+                <li><h3>Product Name1</h3></li>
+                <li>Temporibus autem quibusdam et aut officiis debitis.</li>
+                <li><button>SHOP NOW</button></li>
+              </ul>
+            </li>
+            <li>
+              <img src="http://via.placeholder.com/300X300" alt='bowl' />
+              <ul className='pickedDesc'>
+                <li><h3>Product Name2</h3></li>
+                <li>Ecessitatibus saepe eveniet ut et voluptates repudiandae.</li>
+                <li><button>SHOP NOW</button></li>
+              </ul>
+            </li>
+            <li>
+              <img src="http://via.placeholder.com/300X300" alt='bowl' />
+              <ul className='pickedDesc'>
+                <li><h3>Product Name3</h3></li>
+                <li>Aut perferendis doloribus asperiores repellat.</li>
+                <li><button>SHOP NOW</button></li>
+              </ul>
+            </li>
+            <li>
+              <img src="http://via.placeholder.com/300X300" alt='bowl' />
+              <ul className='pickedDesc'>
+                <li><h3>Product Name4</h3></li>
+                <li>Rerferendis autem quibusdam et au repellat.</li>
+                <li><button>SHOP NOW</button></li>
+              </ul>
+            </li>
+          </ul>
         </article>
       </section>
     </div>
