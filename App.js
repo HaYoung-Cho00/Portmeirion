19561791705e7c265fce00918471e3f03d4ccc50
Hayoung Cho <hayoungcho826@gmail.com>
2022-01-07 19:39:58 +0900

    Update App.js

--- App.js
+++ App.js
@@ -2,12 +2,14 @@ import './App.css';
 import Header from './include/Header';
 import Footer from './include/Footer';
 import Main from './main/main-index';
+import ViewLists from './products/view-lists-index.js';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <Main></Main>
+      {/* <ViewLists></ViewLists> */}
       <Footer></Footer>
     </div>
   );
