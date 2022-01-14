ceedc6302cbf8dddb209714a97c73d672f675fa8
Hayoung Cho <hayoungcho826@gmail.com>
2022-01-14 17:08:19 +0900

    cart page in progress...

--- App.js
+++ App.js
@@ -2,14 +2,18 @@ import './App.css';
 import Header from './include/Header';
 import Footer from './include/Footer';
 import Main from './main/main-index';
-import ViewLists from './products/view-lists-index.js';
+import ViewLists from './products/view-lists-index';
+import Login from './login/Login';
+import Cart from './cart/Cart';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
-      <Main></Main>
-      {/* <ViewLists></ViewLists> */}
+      {/* <Main></Main> */}
+      {/* <ViewLists></ ViewLists> */}
+      {/* <Login></Login> */}
+      <Cart></Cart>
       <Footer></Footer>
     </div>
   );
---
+++ cart/Cart.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+function Cart() {
+    return (
+        <section id='cart' className='innerContainer'>
+            <h1>카트</h1>
+        </section>
+    );
+}
+
+export default Cart;
