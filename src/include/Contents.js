55e5e30a073b60c4493b36ed2a0a9aded913d52c
HY <hayoungcho826@gmail.com>
2022-02-18 09:30:20 +0900

    New  edited

--- src/include/Contents.js
+++ src/include/Contents.js
@@ -23,18 +23,18 @@ function Contents({products}) {
   return(
     <article id='contents'>
       <ul>
-        {products.map((product, index) => (
-            <li key={product.id}>
-                <Link to={`/detailView/${product.id}&${product.collection}`}>
-                <ul className='itemBox'>
-                  <img src={`../../img/${product.imgUrl}.jpg`} alt="products" />
-                  <li className='new'>NEW</li>
-                  <li><h3>{product.name}</h3></li>
-                  <li>${product.price}</li>
-                </ul>
-              </Link>
-            </li>
-          ))}
+        {products.map(product => (
+          <li key={product.id}>
+              <Link to={`/detailView/${product.id}&${product.collection}`}>
+              <ul className='itemBox'>
+                <img src={`../../img/${product.imgUrl}.jpg`} alt="products" />
+                <li className='new'>{product.newArrival == 1? 'NEW': ''}</li>
+                <li><h3>{product.name}</h3></li>
+                <li>${product.price}</li>
+              </ul>
+            </Link>
+          </li>
+        ))}
       </ul>
     </article>
   )
